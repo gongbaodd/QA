6809ed805532c3f7cee9d70646acaa7a19f83a1c
gongbaodd@Inspiron_PS <gongbaodd@qq.com>
2021-10-13 22:45:27 +0800

    10

---
+++ app.mjs
@@ -0,0 +1,32 @@
+import imagemin from "imagemin";
+import imageminJpegtran from "imagemin-jpegtran";
+import imageminPngquant from "imagemin-pngquant";
+import fs from "fs";
+
+const HOST = "https://static.gongbushang.com/";
+
+const files = await imagemin(["public/*.{jpg,png}"], {
+  destination: "images",
+  plugins: [
+    imageminJpegtran(),
+    imageminPngquant({
+      quality: [0.6, 0.8],
+    }),
+  ],
+});
+
+const dests = files
+  .map(({ destinationPath }) => {
+    return destinationPath.replace(/.*\\/, `${HOST}images/`);
+  })
+  .reduce((sum, path, index) => {
+    if (!(index % 2)) {
+      return [...sum, [path]];
+    }
+
+    return [...sum.slice(0, -1), [sum[sum.length - 1][0], path]];
+  }, [])
+  .map((group) => group.join(","))
+  .join("\n");
+
+fs.writeFileSync("dest.csv", dests);
